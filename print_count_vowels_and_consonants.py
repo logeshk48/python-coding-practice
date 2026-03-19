037e3532642d2d3f040f052c9e96ef7fa0d53cdd
Logesh K <139673951+logeshk48@users.noreply.github.com>
2026-03-19 21:53:12 +0530

    Add function to count vowels and consonants

---
+++ print_count_vowels_and_consonants.py
@@ -0,0 +1,23 @@
+a=input().lower()
+vlist=[]
+clist[]
+vowel=0
+con=0
+for ch in a:
+  if ch.isalpha():
+    if ch in 'aeiou':
+      vowel=vowel+1
+      vlist.append(ch)
+    else:
+      con=con+1
+      clist.append(ch)
+
+print(vlist)
+print(vowel)
+print("_______")
+print(clist)
+print(con)
+
+
+
+  
