b35aedb3c1a2f49f2a57e3eae93ab405a97f5b15
Logesh K <139673951+logeshk48@users.noreply.github.com>
2026-03-25 21:26:20 +0530

    Add script to find the second largest number

---
+++ Second Largest Number.py
@@ -0,0 +1,12 @@
+arr = list(map(int, input().split()))
+
+first = second = -999999
+
+for num in arr:
+    if num > first:
+        second = first
+        first = num
+    elif num > second and num != first:
+        second = num
+
+print(second)
